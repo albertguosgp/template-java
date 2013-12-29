4bde9650e7261c3065c469405865fd6effa5be5b
Albert <xjtuguochen@gmail.com>
2013-12-29 22:52:25 +0800

    initial import.

---
+++ src/main/java/com/durian/controller/RegisterController.java
@@ -0,0 +1,12 @@
+package com.durian.controller;
+
+/**
+ * User: johndoe
+ * Date: 12/29/13
+ * Time: 10:51 PM
+ */
+public class RegisterController {
+    public static void main(String[] args) {
+        System.out.print("hello");
+    }
+}
